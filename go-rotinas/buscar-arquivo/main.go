c5c9684ce482ea1d8070cb2ce2b2372279191a5f
lucas <lucascplusmart@gmail.com>
2022-10-09 15:45:44 -0300

    [golang] busca-arquivo-go-rotinas-inputs

--- go-rotinas/buscar-arquivo/main.go
+++ go-rotinas/buscar-arquivo/main.go
@@ -44,11 +44,10 @@ func obterLista(s string) []string {
 	return strings.Split(s, ",")
 }
 
-func processarBuscar(lista []string, controleRotina *sync.WaitGroup, canal chan string) {
-	// defer controleRotina.Done()
-
+func processarBuscar(lista []string, controleRotina *sync.WaitGroup, valorBusca string, canal chan string) {
+	defer controleRotina.Done()
 	for i := range lista {
-		if lista[i] == "10" {
+		if lista[i] == valorBusca {
 			canal <- lista[i]
 		}
 	}
@@ -57,34 +56,39 @@ func processarBuscar(lista []string, controleRotina *sync.WaitGroup, canal chan 
 
 func main() {
 
-	var controleRotina sync.WaitGroup
+	var (
+		controleRotina sync.WaitGroup
+		valorBusca     string
+	)
 
 	data := lerArquivo("teste.txt")
-
 	lista := obterLista(string(data))
-
 	superLista := particionarLista(lista, MAX_GOROTINAS)
-
 	canal := make(chan string, 0)
 
-	// for i := range superLista {
-	// 	// controleRotina.Add(1)
-	// 	go processarBuscar(superLista[i], &controleRotina, canal)
-	// }
-
-	go processarBuscar(superLista[0], &controleRotina, canal)
-	go processarBuscar(superLista[1], &controleRotina, canal)
-
-	select {
-	case <-canal:
-		fmt.Println("Canal", <-canal)
+	fmt.Println("Informe o valor de busca:")
+	n, erro := fmt.Scanln(&valorBusca)
+	if erro != nil {
+		log.Println("Erro ao ler valor de busca:", erro)
+		return
 	}
 
-	// go func() {
-	// 	controleRotina.Wait()
-	// 	close(canal)
-	// }()
+	for i := range superLista {
+		controleRotina.Add(1)
+		go processarBuscar(superLista[i], &controleRotina, valorBusca, canal)
+	}
 
-	// fmt.Println("Dados:", superLista)
+	go func() {
+		controleRotina.Wait()
+		close(canal)
+	}()
+
+	valor, existeValor := <-canal
+
+	if existeValor {
+		fmt.Println("valor encontrado:", valor, n)
+	} else {
+		fmt.Println("valor não encontrado!")
+	}
 
 }
